0b6528ebf6b64278dcd7f3d7171e7215ac012d4d
thomasryu <thomas.sameshima@gmail.com>
2023-10-03 11:13:35 -0300

    Adding lesson 8 challenges

---
+++ lessons/L8_Dominator.js
@@ -0,0 +1,67 @@
+/*
+
+An array A consisting of N integers is given.
+The dominator of array A is the value that occurs in more than half of the elements of A.
+
+For example, consider array A such that
+  A[0] = 3    A[1] = 4    A[2] =  3
+  A[3] = 2    A[4] = 3    A[5] = -1
+  A[6] = 3    A[7] = 3
+
+The dominator of A is 3 because it occurs in 5 out of 8 elements of A (namely in those
+  with indices 0, 2, 4, 6 and 7)and 5 is more than a half of 8.
+
+Write a function
+  function solution(A);
+
+that, given an array A consisting of N integers, returns index of any element of array A in which the dominator of A occurs. The function should return −1 if array A does not have a dominator.
+
+For example, given array A such that
+  A[0] = 3    A[1] = 4    A[2] =  3
+  A[3] = 2    A[4] = 3    A[5] = -1
+  A[6] = 3    A[7] = 3
+
+the function may return 0, 2, 4, 6 or 7, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+- N is an integer within the range [0..100,000];
+- each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+
+Copyright 2009–2023 by Codility Limited. All Rights Reserved.
+Unauthorized copying, publication or disclosure prohibited.
+
+*/
+
+function solution(A) {
+  const countStack = []
+
+  A.forEach((a) => {
+    const b = countStack.pop()
+    if (b == null) {
+      countStack.push(a)
+    }
+    if (b && b == a) {
+      countStack.push(b)
+      countStack.push(a)
+    }
+  })
+
+  const N = A.length
+
+  const candidate = countStack.pop()
+  let candidateCount = 0
+
+  let result = -1
+
+  A.forEach((a, i) => {
+    if (a == candidate) {
+      candidateCount++
+
+      if (candidateCount > N / 2) {
+        result = i
+      }
+    }
+  })
+
+  return result
+}
